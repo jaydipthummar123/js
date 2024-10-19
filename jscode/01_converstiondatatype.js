e32469d97dbac82e9a3accea886e4f6db081d689
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-19 07:09:16 +0000

    cc

--- jscode/01_converstiondatatype.js
+++ jscode/01_converstiondatatype.js
@@ -1,3 +1,13 @@
 let Num="33"
  let stringname = Number(Num)
 console.log( typeof Num);
+
+// let name1="jaydip"
+// let name2 =" thummar"
+// let name3 = name1 + name2
+
+// console.log(name3)
+
+
+// let n = " jaydip ";
+// console.log(`hello mu name is${n}`);
