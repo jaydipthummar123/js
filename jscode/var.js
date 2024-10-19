60a95dbdb80174a8a09c0d7001ecd6c6ea476833
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-19 05:46:35 +0000

    varchang

---
+++ jscode/var.js
@@ -0,0 +1,13 @@
+const AccuntiD = 23434;
+let AccountName = "jaydip";
+var AccountSalary = "20000";
+AccountCit = "jaipur";
+let company
+
+
+AccountName = "monil";
+AccountSalary = "30000";
+AccountCit = "gujrat"
+
+console.log(AccuntiD);
+console.table([AccuntiD, AccountName, AccountSalary, AccountCit, company])
