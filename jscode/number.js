8c65ef0a3bcc24b32e1d73887fd7bfb9a0912812
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-21 07:33:43 +0000

    number

---
+++ jscode/number.js
@@ -0,0 +1,20 @@
+// const score = 594748957
+// // score =4564;
+// console.log(score.toString())
+// const balane = new Number(200)
+// console.log(balane);
+// console.log(balane.toString());
+// console.log(balane.toFixed(2));
+
+
+
+// const number = 100000
+// console.log(number.toLocaleString())
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// Math.js
+ 
+// const number =26
+console.log(Math.random())
+// console.log(Math.round(number),(Math.abs(number)))
