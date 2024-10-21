0fe899f5a4dad09133fa0836e94205f8bdfc72d7
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-21 11:51:02 +0000

    date

---
+++ jscode/date_and _time.js
@@ -0,0 +1,13 @@
+const date =new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+
+// ================= custom date ++++++++++++++++
+ const mydate = new Date(2024,9,21)
+ console.log(mydate.toLocaleDateString());
+
+ const Mydate = new Date(2024,9,2,21,56,32)
+ console.log(Mydate.toLocaleString());
