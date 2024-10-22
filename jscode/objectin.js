196e551ecb566620558c7c50c81b05d46d2ea0ca
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-22 07:08:39 +0000

    objectapi

---
+++ jscode/objectin.js
@@ -0,0 +1,25 @@
+const course ={
+    courseName: "javascript lern",
+    price : 30000,
+    courseInsructer: "jaydip Thummar"
+}
+const {price} = course
+console.log(price)
+console.log(course)
+
+// ++++++++++++++++++JSOn++++++++++++++
+
+// object api
+{
+    "name":"jaydipthummar",
+    "age":21,
+    "qulification":"BCA"
+}
+
+
+// arrry api 
+[
+    {},
+    {},
+    {}
+]
