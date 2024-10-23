6b20abea69b00c5351ee589fd521e1ed556b3b41
jaydipthummar123 <jaydipthummar47@gmail.com>
2024-10-23 05:05:17 +0000

    iife

---
+++ jscode/ifee.js
@@ -0,0 +1,17 @@
+//  function chai (){
+//     console.log(" u drink chai ")
+//  }
+// chai()
+
+
+//  +++++++IIFE+++++++
+// named iife 
+// (function chai (){
+//     console.log(" u drink chai ")
+//  })()
+
+
+ ((name)=>{
+    console.log(`hello my name ${name}`)
+ })("jaydip thummar");
+
